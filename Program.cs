0b5b2945a2df96e44b683f9876d0df35253682a5
Kevin Gomez <kevin.gomezzacarias.579@my.csun.edu>
2022-04-20 00:02:26 -0500

    created a c# program

---
+++ Program.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Giraffe1{
+    class Program{
+        static void Main(string [] args){
+            Console.WriteLine("hello world!");            
+            Console.ReadLine();
+        }
+    }
+}
